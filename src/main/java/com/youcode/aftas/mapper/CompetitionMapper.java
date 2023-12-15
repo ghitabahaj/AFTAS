0bfb9c47d91dbe96751802ce115fe771d415aebd
Ghita <bahajghita3@gmail.com>
2023-12-15 12:22:23 +0100

    some updates

---
+++ src/main/java/com/youcode/aftas/mapper/CompetitionMapper.java
@@ -0,0 +1,19 @@
+package com.youcode.aftas.mapper;
+
+import com.youcode.aftas.DTO.CompetitionDTO;
+import com.youcode.aftas.entities.Competition;
+
+public class CompetitionMapper {
+    public static CompetitionDTO mapToDto(Competition competition){
+        return CompetitionDTO.builder()
+                .id(competition.getId())
+                .code(competition.getCode())
+                .date(competition.getDate())
+                .startTime(competition.getStartTime())
+                .amount(competition.getAmount())
+                .endTime(competition.getEndTime())
+                .location(competition.getLocation())
+                .numberOfParticipants(competition.getNumberOfParticipants())
+                .build();
+    }
+}
